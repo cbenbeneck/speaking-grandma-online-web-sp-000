1ee644fa7ddca5326cc977175ca52aef554d988c
Christina Benbeneck <cbenbeneck@mail.usf.edu>
2018-10-18 15:54:02 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,9 @@
 def speak_to_grandma(string)
   if string == string.upcase
     return "NO, NOT SINCE 1938!"
-  else return false 
+    elsif string == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN"
+  else return "HUH?! SPEAK UP, SONNY!"
   end
 end
 
