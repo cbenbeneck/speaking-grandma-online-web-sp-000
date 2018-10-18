8bd545e5206fcd7a3745c99a0615b61d39118a6b
Christina Benbeneck <cbenbeneck@mail.usf.edu>
2018-10-18 15:50:20 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,12 +13,9 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(string)
-  string == gets.strip
+  string = gets.strip
   if string == string.upcase
-    
     return "NO, NOT SINCE 1938!"
-    elsif string == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
   else return "HUH? SPEAK UP, SONNY!"
   end
 end
