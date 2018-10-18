830c9f0d579b42b1cf1b0447c925a26427d7b42c
Christina Benbeneck <cbenbeneck@mail.usf.edu>
2018-10-18 15:39:49 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(string)
-  if string.upcase
+  if gets.strip.upcase
     puts "NO, NOT SINCE 1938!"
     elsif string == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
